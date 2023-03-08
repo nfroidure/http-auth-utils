bfb33f4c22f6596258343505ecdd5205d9a75309
corentin <corentin.girard@datadoghq.com>
2023-03-08 13:28:18 +0100

    fix(src/utils.ts): normalize keys to lowercase

--- src/utils.ts
+++ src/utils.ts
@@ -40,8 +40,9 @@ export function parseHTTPHeadersQuotedKeyValueSet(
       return pair;
     })
     .reduce(function (parsedValues, [name, value], valuePosition) {
-      if (-1 === authorizedKeys.indexOf(name)) {
-        throw new YError('E_UNAUTHORIZED_KEY', valuePosition, name);
+      const normalizedName = name.toLowerCase();
+      if (-1 === authorizedKeys.indexOf(normalizedName)) {
+        throw new YError('E_UNAUTHORIZED_KEY', valuePosition, normalizedName);
       }
       /*
        * Regular expression for stripping paired starting and ending double quotes off the value:
@@ -54,7 +55,7 @@ export function parseHTTPHeadersQuotedKeyValueSet(
        * "      = The ending double quote
        * $      = The end of the string
        */
-      parsedValues[name] = value.replace(/^"(.+(?="$))"$/, '$1');
+      parsedValues[normalizedName] = value.replace(/^"(.+(?="$))"$/, '$1');
       return parsedValues;
     }, {});
 
